6ad1906488bd5dfb002cb2a04fb2359c13fec421
George Zacharias <zachariasg7@hotmail.gr>
2025-10-27 08:52:35 +0200

    avatar import and start sections

--- react-portofolio/src/App.jsx
+++ react-portofolio/src/App.jsx
@@ -1,11 +1,13 @@
 
 import styles from "./App.module.css";
 import { Navbar } from "./componets/NavBar/NavBar";
+import { Avatar } from "./componets/Avatar/Avatar";
 function App() {
 
   return (
     <div className={styles.App}>
       <Navbar />
+      <Avatar />
     </div>
   )
 }
---
+++ react-portofolio/src/componets/Avatar/Avatar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+export const Avatar = () => {
+
+
+    return (<section>
+        <div><H1> TEST</H1></div>
+    </section>
+
+    );
+};
