82494c7aa4091f111b9884d4faba19bb578ba1a7
George Zacharias <zachariasg7@hotmail.gr>
2025-10-27 06:15:53 +0200

    WORKS GOOD

--- react-portofolio/src/componets/NavBar/NavBar.jsx
+++ react-portofolio/src/componets/NavBar/NavBar.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Navbar.module.css";
+import { getImageUrl } from "../../utils"
 
 export const Navbar = () => {
     return <nav className={styles.navbar}>
         <a className={styles.title} href="/">Portofolio</a>
         <div className={styles.menu}>
             <ul className={styles.menuItems}>
+
                 <li>
                     <a href="#about">About</a>
                 </li>
