770d8845fe33196cec553955fdcbeda5c0b33cac
George Zacharias <zachariasg7@hotmail.gr>
2025-10-27 05:58:11 +0200

    navar layout

--- react-portofolio/src/componets/NavBar/NavBar.jsx
+++ react-portofolio/src/componets/NavBar/NavBar.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import styles from "./Navbar.module.css";
 
 export const Navbar = () => {
-    return <div>Navbar</div>
+    return <nav className={styles.navbar}>
+        <a className={styles.title} href="/">Portofolio</a>
+        <div className={styles.menu}>
+            <ul className={styles.menuItems}>
+                <li>
+                    <a href="#about">About</a>
+                </li>
+                <li>
+                    <a href="#experience">Experience</a>
+                </li>
+                <li>
+                    <a href="#projects">Projects</a>
+                </li>
+                <li>
+                    <a href="#about">Contacts</a>
+                </li>
+            </ul>
+
+        </div>
+    </nav>
 }
