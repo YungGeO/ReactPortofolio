66c26745603736fb45d1ee819f4c4e069b2d7dc9
George Zacharias <zachariasg7@hotmail.gr>
2025-10-27 05:43:44 +0200

    setting up navbar

--- react-portofolio/src/App.jsx
+++ react-portofolio/src/App.jsx
@@ -1,10 +1,11 @@
 
 import styles from "./App.module.css";
+import { Navbar } from "./componets/NavBar/NavBar";
 function App() {
 
   return (
     <div className={styles.App}>
-
+      <Navbar />
     </div>
   )
 }
