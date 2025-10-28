05059dd6755b79149d99d8fc05030f8fe2ca39e4
George Zacharias <zachariasg7@hotmail.gr>
2025-10-28 09:47:38 +0200

    text in the about

--- react-portofolio/src/componets/About/About.jsx
+++ react-portofolio/src/componets/About/About.jsx
@@ -8,8 +8,21 @@ export const About = () => {
 
 
     return (
-        <section className={styles.container}>
-
+        <section className={styles}>
+            <li >
+                <h2></h2>
+                <div>
+                    <h3>Software  Engineer</h3>
+                    <p>I have a batcherlor in software engineering</p>
+                </div>
+            </li>
+            <li >
+                <h2></h2>
+                <div>
+                    <h3>Who I Am</h3>
+                    <p>I am George Zacharias, i am 26 years old and i aspire to become a full stack developer</p>
+                </div>
+            </li>
         </section>
 
     );
