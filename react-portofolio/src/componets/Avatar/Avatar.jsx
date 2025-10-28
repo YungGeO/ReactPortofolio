a813ce65962f402f9cc2f14cef3fe0d981ee7fc0
George Zacharias <zachariasg7@hotmail.gr>
2025-10-28 07:08:35 +0200

    updating what i am doing at the moment

--- react-portofolio/src/componets/Avatar/Avatar.jsx
+++ react-portofolio/src/componets/Avatar/Avatar.jsx
@@ -12,8 +12,7 @@ export const Avatar = () => {
             <div className={styles.content}>
                 <h1 className={styles.title}> Hi, I'm George</h1>
                 <p className={styles.description}>Hi i am George i am a junior software developer
-                    with no job Experience but with thirst to learn
-                    and to become a great programmer.
+                    certainly working on a full stack course from coursera.
                 </p>
                 <a className={styles.contactBtn}
                     href="mailto:zachariasg7@hotmail.gr">Contact Me
