2efb33ad82a1536b8d59a9d738aa27218f85eb13
George Zacharias <zachariasg7@hotmail.gr>
2025-10-30 10:46:14 +0200

    setting up Projetcs section

--- react-portofolio/src/App.jsx
+++ react-portofolio/src/App.jsx
@@ -4,6 +4,7 @@ import { Navbar } from "./componets/NavBar/NavBar";
 import { Avatar } from "./componets/Avatar/Avatar";
 import { About } from "./componets/About/About";
 import { Skills } from "./componets/Skills/Skills";
+// https://georgethema.infinityfreeapp.com/
 function App() {
 
   return (
---
+++ react-portofolio/src/componets/Projects/Projects.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import skills from "../../data/skills.json";
+import { getImageUrl } from "../../utils";
+import styles from "./Skills.module.css";
+
+export const Skills = () => {
+    return (
+        <section className={styles.container} id="skills">
+            <h2 className={styles.title}>Projects</h2>
+
+        </section>
+    );
+};
