aa62c04b2932bfe830321b35031172397e06975e
George Zacharias <zachariasg7@hotmail.gr>
2025-10-30 12:06:44 +0200

    code ready time to css

--- react-portofolio/src/componets/Contact/Contact.jsx
+++ react-portofolio/src/componets/Contact/Contact.jsx
@@ -14,6 +14,14 @@ export const Contact = () => {
                     <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
                     <a href="mailto:zachariasg7@hotmail.com">zachariasg7@hotmail.com</a>
                 </li>
+                <li >
+                    <img src={getImageUrl("contact/linkedinIcon.png")} alt="LikedIn icon" />
+                    <a href="https://www.linkedin.com/in/yunggeo">linkedin.com/in/yunggeo</a>
+                </li>
+                <li >
+                    <img src={getImageUrl("contact/githubIcon.png")} alt="GitHub icon" />
+                    <a href="https://github.com/YungGeO">github.com/YungGeO</a>
+                </li>
             </ul>
         </footer>
     );
