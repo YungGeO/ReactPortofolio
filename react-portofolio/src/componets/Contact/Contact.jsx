d4bff6e67ed07278d5e587cd4f0768ba64f74968
George Zacharias <zachariasg7@hotmail.gr>
2025-10-30 12:02:47 +0200

    nearly done i guess

--- react-portofolio/src/componets/Contact/Contact.jsx
+++ react-portofolio/src/componets/Contact/Contact.jsx
@@ -3,5 +3,18 @@ import styles from "./Contact.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Contact = () => {
-    return (<p>hello</p>);
+    return (
+        <footer id="contact" >
+            <div >
+                <h2>Contact</h2>
+                <p>Feel free to reach out!</p>
+            </div>
+            <ul>
+                <li >
+                    <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+                    <a href="mailto:zachariasg7@hotmail.com">zachariasg7@hotmail.com</a>
+                </li>
+            </ul>
+        </footer>
+    );
 }
